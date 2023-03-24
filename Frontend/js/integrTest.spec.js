fcbe4caa09d527a051cfc24c6c5ba4e5015f8b60
Nazarenko Alena <81744676+Nazarenko-Alena@users.noreply.github.com>
2023-03-24 13:35:28 +0300

    Update integrTest.spec.js

--- Frontend/js/integrTest.spec.js
+++ Frontend/js/integrTest.spec.js
@@ -192,7 +192,7 @@ describe("Scenario 15 - Search result by keyword", () => {
     before(async ()=>{
         browserPup = await puppeteer.launch();
         page = await browserPup.newPage();
-        await page.goto('file:///home/runner/work/Povarenok/Povarenok/Frontend/dist/index.html',{waitUntil: 'domcontentloaded'});
+        await page.goto('file:///home/runner/work/Povarenok/Povarenok/Frontend/dist/index.html');
         //await browser.get('file:///home/runner/work/Povarenok/Povarenok/Frontend/dist/index.html');
         //let searchLine = await browser.wait(
           //  until.elementLocated(By.id('searchLine')), 10000);
